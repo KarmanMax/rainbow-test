50bc3da157444b2cdb3832bc9cc13fe8593e1be9
karman <minxxx0926@gmail.com>
2024-06-19 11:43:46 +0800

    update coinbase smart wallet

--- src/RainbowKit/getChainsConfig.ts
+++ src/RainbowKit/getChainsConfig.ts
@@ -1,19 +1,20 @@
 // import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { connectorsForWallets } from '@rainbow-me/rainbowkit'
-import { injectedWallet, coinbaseWallet, walletConnectWallet } from '@rainbow-me/rainbowkit/wallets'
+import { injectedWallet, walletConnectWallet } from '@rainbow-me/rainbowkit/wallets'
 import { createConfig, http } from 'wagmi'
 import { optimism } from 'wagmi/chains'
+import { coinbaseSmartWallet } from './coinbaseSmartWallet'
 
 const appName = 'ExtraX'
 const projectId = 'ae9bc6c16bf3d9121367f023f740150a' // Project ID of WalletConnect Cloud
 
-;(coinbaseWallet as any).preference = 'smartWalletOnly'
+// ;(coinbaseWallet as any).preference = 'smartWalletOnly'
 
 const connectors = connectorsForWallets(
   [
     {
       groupName: 'Recommended',
-      wallets: [injectedWallet, coinbaseWallet, walletConnectWallet],
+      wallets: [injectedWallet, coinbaseSmartWallet, walletConnectWallet],
     },
   ],
   {
